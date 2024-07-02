b94285ed2177b71dd8d02d765afaaaf10ed55a5c
RREA07 <hyzoeyu@utexas.edu>
2024-07-01 23:57:31 -0500

    Added fully functioning menus, background music, frameworks for sound effects and more features.
    I will work on player health, fall zone, and enemies tomorrow.

---
+++ Assets/Scripts/Logic/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Quits game
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    //Starts game
+    public void gameStart()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
