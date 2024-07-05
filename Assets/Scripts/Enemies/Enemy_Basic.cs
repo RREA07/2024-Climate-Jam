42653c1613fa45a33c215f3d6fb63a5151276a74
RREA07 <hyzoeyu@utexas.edu>
2024-07-05 03:40:10 -0500

    Basic enemy patrol, dialogues.

--- Assets/Scripts/Enemies/Enemy_Basic.cs
+++ Assets/Scripts/Enemies/Enemy_Basic.cs
@@ -11,6 +11,11 @@ public class Enemy_Basic : MonoBehaviour, Damage
     // Internal variables
     private Rigidbody2D rb;
     private bool isGrounded;
+    public GameObject pointA;
+    public GameObject pointB;
+    private Animator animator;
+    private Transform currentPoint;
+    public float speed = 2f;
 
     // Ground check variables
     public UnityEngine.Transform groundCheck;
@@ -22,6 +27,7 @@ public class Enemy_Basic : MonoBehaviour, Damage
     {
         currentHealth = 3f;
         rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
     }
 
     // Update is called once per frame
@@ -30,6 +36,26 @@ public class Enemy_Basic : MonoBehaviour, Damage
         //Enemy Ground Check
         bool previousGrounded = isGrounded;
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        //Path finding
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
     }
 
     //Taking damge from player
@@ -53,5 +79,13 @@ public class Enemy_Basic : MonoBehaviour, Damage
     public void defeat()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
 }
